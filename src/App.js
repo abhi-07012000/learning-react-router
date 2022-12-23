34e0f82f27d8c6db41dfb55fc26456ab889701d6
AbhijeetMane <abhijeetmane@kali.kali>
2022-12-23 20:35:30 +0530

    when we type wroong route for ex:localhost:3000/gear -> page no found components render || path="*" has some meaning if no routes mathes this will runs  DONE.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 
 import './App.css';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import About from './Pages/About';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
+import NAvbar from './Pages/NAvbar';
+import OrderSumarry from './Pages/OrderSumarry';
+import NoMatch from './Pages/NoMatch';
 
 function App() {
-  return (
-    <BrowserRouter>
+  return (<>
+      <NAvbar />
       <Routes>
 
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About /> } />
         <Route path='/contact' element={<Contact />} />
-
+        <Route path='order-summury' element={<OrderSumarry />}/>
+        <Route path='*' element={<NoMatch /> }/>
       </Routes>
-    </BrowserRouter>
+      </>
   );
 }
 
